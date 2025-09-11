89f9f0c5ce56f60ae9e94ade66f3140ee917e6ee
3Thirty3 Ltd. <threethirtythreeprinting@gmail.com>
2025-09-11 17:34:08 +0000

    Try fixing this error: `Console Error: A component is changing an uncont

--- src/app/admin/products/new/page.tsx
+++ src/app/admin/products/new/page.tsx
@@ -116,6 +116,8 @@ export default function NewProductPage() {
       allowOutOfStockPurchase: false,
       seoTitle: '',
       seoDescription: '',
+      compareAtPrice: undefined,
+      costPerItem: undefined,
     },
   });
 
